401ccd907b5ca11ffd27583a9b011e4ea55afb27
Kevin Lopez <kalopezo@unal.edu.co>
2018-05-23 21:45:17 -0500

    Agregando manejo de excepciones a los métodos.

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@
 public class Main {
 
 	public static void main (String [] args) {
-		/*Numero num1 = new Numero(30);
-		System.out.print(num1);*/
-		Numero.imprimirRango(1,100);
-
+		System.out.print(String.valueOf(null));
+		//Numero.imprimirRango(1,100);
     } 
 
 }
--- src/Numero.java
+++ src/Numero.java
@@ -3,7 +3,6 @@ public class Numero {
 
 	private int valor;
 	public Numero(int valor) {
-		// TODO Auto-generated constructor stub
 		this.setValor(valor);
 	}
 	public int getValor() {
@@ -14,26 +13,38 @@ public class Numero {
 	}
 	
 	public String toString() {
-		if(valor % 5 == 0 && valor % 3 == 0) {
-			return "FizzBuzz";
-		}else if(valor % 3 == 0){
-			return "Fizz";
-		}else if(valor % 5 == 0) {
-			return "Buzz";
-		}else {
-			return String.valueOf(valor);
+		try {
+			if(valor % 5 == 0 && valor % 3 == 0) {
+				return "FizzBuzz";
+			}else if(valor % 3 == 0){
+				return "Fizz";
+			}else if(valor % 5 == 0) {
+				return "Buzz";
+			}else {
+				return String.valueOf(valor);
+			}
+		}catch(NullPointerException e){
+			 return "Valor del número nulo.";
+		}catch(Exception e) {
+			return "Error " + e;
 		}
-		
 	}
+		
 	public static void imprimirRango(int inferior, int superior) {
-		StringBuffer salida = new StringBuffer();
-		Numero numero;
-		for (int k = inferior; k <= superior; k++) {
-			numero = new Numero(k);
-			salida.append(numero);
-			salida.append("\n");
+		try {
+			StringBuffer salida = new StringBuffer();
+			Numero numero;
+			for (int k = inferior; k <= superior; k++) {
+				numero = new Numero(k);
+				salida.append(numero);
+				salida.append("\n");
+			}
+			System.out.print(salida);
+		}catch(NullPointerException e){
+			 System.out.println("Valor del número nulo.");
+		}catch(Exception e) {
+			System.out.println("Error " + e);
 		}
-		System.out.print(salida);
 	}
 
 }
